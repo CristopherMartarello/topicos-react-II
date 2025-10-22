38402756560b3cb6ffab5cc256d82716f17b9892
CristopherMartarello <crisrossi1313@gmail.com>
2025-10-22 15:03:27 -0300

    chore: setup project, with react router, tailwind and antd

---
+++ online-shop/src/pages/layout/AppLayout.tsx
@@ -0,0 +1,48 @@
+import { Layout, theme } from "antd";
+import { Outlet } from "react-router-dom";
+import OnlineShopLogo from "../../assets/online-shop-logo.png";
+
+const { Header, Content, Footer } = Layout;
+
+export default function AppLayout() {
+  const { token } = theme.useToken();
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header
+        className="flex items-center shadow-md"
+        style={{
+          backgroundColor: token.colorPrimary,
+        }}
+      >
+        <img
+          src={OnlineShopLogo}
+          alt="Online Shop Logo"
+          className="mr-4 h-12 w-12"
+        />
+        <div className="text-2xl font-bold text-white">
+          <h1>Pokedex</h1>
+        </div>
+      </Header>
+
+      <Content
+        className="p-6"
+        style={{
+          backgroundColor: token.colorBgContainer,
+        }}
+      >
+        <Outlet />
+      </Content>
+
+      <Footer
+        className="flex justify-center"
+        style={{
+          color: token.colorTextSecondary,
+        }}
+      >
+        Pokedex | Looqbox ©{new Date().getFullYear()} Created by Cristopher
+        Martarello
+      </Footer>
+    </div>
+  );
+}
